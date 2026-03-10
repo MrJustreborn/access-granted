935af26e60caa02a27e8323a1fa4eb53c7317975
René Lottes <rl@bitrot.eu>
2026-03-10 16:24:28 +0100

    Dockerize

--- src/main.py
+++ src/main.py
@@ -37,4 +37,4 @@ def index():
     return render_template("index.html", client_ip=client_ip, subnet=subnet)
 
 if __name__ == "__main__":
-    app.run(host="::", port=5000)
+    app.run(host="0.0.0.0", port=5000)
